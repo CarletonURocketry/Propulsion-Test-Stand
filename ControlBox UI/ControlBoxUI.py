8cde4d844907a694c76a529984818c9e42f2b9f6
angelachenn <angela.c1331@gmail.com>
2023-08-13 19:41:27 -0400

    actual initial commit

--- ControlBox UI/ControlBoxUI.py
+++ ControlBox UI/ControlBoxUI.py
@@ -1,161 +1,107 @@
-import pyqtgraph as pg
-from pyqtgraph.Qt import QtGui, QtCore
-import random
+"""The Test Stand application is intended to be used for the Carleton CUInSpace rocket team test stand
+for the new hybrid rocket engine.
+
+Author: Michael Marsland (michaelmarsland@cmail.carleton.ca)
+Date: June 11th, 2022
+"""
+# Be sure to use the local subpackage rather than any globally-installed version.
+import local_pyqtgraph.pyqtgraph as pg
+from pyqtgraph.Qt import QtCore
+# Serial
+from pySerialTransfer import pySerialTransfer as txfer
+# Other
 from datetime import datetime
-import os
+import numpy as np
+import time, os, math, random
 
-class Data():
-    def __init__(self):
-        self.millisSince = 0
-        self.L1 = 0
-        self.P1 = 0
-        self.P2 = 0
-        self.P3 = 0
-        self.P4 = 0
-        self.T1 = 0
-        self.Safe = True
+#Debuging variables
+lastStatus = -1
 
-class Plot():
-    def __init__(self, parent=None):
-        self.parent = parent
-        self.app = QtGui.QApplication([])
-        self.win = pg.GraphicsWindow(title="Plot")
-        self.plot = self.win.addPlot(title="My Plot")
-        self.curve = self.plot.plot(pen='r')
+plot = pg.plot()
+plot.setWindowTitle('ControlBoxUI')
+plot.setLabel('bottom', 'Time', units='s')
+plot.setLabel('left', 'Pressure', units='PSI')
+plot.showGrid(x=True, y=True)
+nPlots = 6
+# curves = [loadcell, pressure1, pressure2, pressure3, pressure4, temperature]
+curves = []
+for idx in range(nPlots):
+    curve = pg.PlotCurveItem(pen=({'color': (idx, nPlots*1.3), 'width': 1}), skipFiniteCheck=True)
+    plot.addItem(curve)
+    curve.setPos(0,idx*6)
+    curves.append(curve)
 
-    def update(self, x, y):
-        self.curve.setData(x, y)
-        self.app.processEvents()
-
-class PlotSet():
-    def __init__(self, parent=None):
-        self.parent = parent
-        self.plot = Plot(parent=self.parent)
-
-    def update(self, time, data):
-        x = [time.timestamp()]
-        y = [data.T1]
-        self.plot.update(x, y)
-
-class ScaleSlider():
-    def __init__(self, parent, name, minVal, maxVal, step, value, callback):
-        self.parent = parent
-        self.callback = callback
-        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
-        self.slider.setMinimum(minVal)
-        self.slider.setMaximum(maxVal)
-        self.slider.setSingleStep(step)
-        self.slider.setValue(value)
-        self.slider.valueChanged.connect(self.valueChanged)
-        self.label = QtGui.QLabel(name)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.layout = QtGui.QVBoxLayout()
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.slider)
-        self.parent.setLayout(self.layout)
-
-    def valueChanged(self, value):
-        self.callback(value)
-
-class LabeledToggle():
-    def __init__(self, parent, id, callback, command, armed_state_var):
-        self.parent = parent
-        self.id = id
-        self.callback = callback
-        self.command = command
-        self.armed_state_var = armed_state_var
-        self.button = QtGui.QPushButton(f"Solenoid {id}")
-        self.button.setCheckable(True)
-        self.button.clicked.connect(self.clicked)
-        self.layout = QtGui.QVBoxLayout()
-        self.layout.addWidget(self.button)
-        self.parent.setLayout(self.layout)
-
-    def clicked(self):
-        if self.button.isChecked():
-            self.callback(self.command)
-        else:
-            self.callback(f"{self.command}0")
-
-class App(QtGui.QMainWindow):
-    def __init__(self, arduino, logger):
+""" App is the main component for the application. It is the main tkinter window as well as
+the main running code for the application
+"""
+class App():
+    def __init__(self, arduino, refreshRate, debug = False):
         super().__init__()
+        self.data = np.zeros((7,0))
         self.arduino = arduino
-        self.logger = logger
-        self.plotSet = PlotSet(self)
-        self.initUI()
+        self.lastDisplayTime = datetime.now()
+        self.refreshRate = refreshRate
+        self.start_time = datetime.now()
 
-    def initUI(self):
-        self.setWindowTitle("Test Stand Application")
-        self.setGeometry(100, 100, 800, 600)
+        indicators = [[0,"Error","red",1],[0,"Warning","yellow",2],[0,"Waiting","blue",256],[0,"Packet","blue",512],
+                [0,"Armed","yellow",4],[0,"Active","yellow",8],[0,"Abort","red",16],[0,"Invalid","red",32],
+                [0,"Mismatch","red",1024],[0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],
+                [0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],
+                [0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],
+                [0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],
+                [0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],
+                [0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0],[0,"Unused","red",0]]
+        
+        self.indicators = indicators
 
-        self.centralWidget = QtGui.QWidget()
-        self.setCentralWidget(self.centralWidget)
+        #self.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.gridLayout = QtGui.QGridLayout(self.centralWidget)
-
-        self.buttonsFrame = QtGui.QFrame(self.centralWidget)
-        self.buttonsFrame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.buttonsFrame.setFrameShadow(QtGui.QFrame.Raised)
-        self.gridLayout.addWidget(self.buttonsFrame, 0, 1, 1, 1)
-
-        self.temperatureFrame = QtGui.QFrame(self.centralWidget)
-        self.temperatureFrame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.temperatureFrame.setFrameShadow(QtGui.QFrame.Raised)
-        self.gridLayout.addWidget(self.temperatureFrame, 0, 0, 1, 1)
-
-        self.temperatureReadout = QtGui.QLabel(self.temperatureFrame)
-        self.temperatureReadout.setText("0.00")
-        self.temperatureReadout.setAlignment(QtCore.Qt.AlignCenter)
-        self.temperatureReadout.setStyleSheet("font-size: 48pt;")
-        self.temperatureReadout.setGeometry(0, 0, 200, 100)
-
-        self.scaleSlider = ScaleSlider(self.buttonsFrame, "Scale", 0, 100, 1, 50, self.slider)
-        self.solenoid11_toggle = LabeledToggle(self.buttonsFrame, id=11, callback=self.arduino.sendCommand, command="10", armed_state_var=self.armed_state_var)
-        self.solenoid11_toggle.layout.addWidget(self.solenoid11_toggle.button)
-        self.solenoid12_toggle = LabeledToggle(self.buttonsFrame, id=12, callback=self.arduino.sendCommand, command="11", armed_state_var=self.armed_state_var)
-        self.solenoid12_toggle.layout.addWidget(self.solenoid12_toggle.button)
-        self.solenoid13_toggle = LabeledToggle(self.buttonsFrame, id=13, callback=self.arduino.sendCommand, command="12", armed_state_var=self.armed_state_var)
-        self.solenoid13_toggle.layout.addWidget(self.solenoid13_toggle.button)
-        self.solenoid14_toggle = LabeledToggle(self.buttonsFrame, id=14, callback=self.arduino.sendCommand, command="13", armed_state_var=self.armed_state_var)
-        self.solenoid14_toggle.layout.addWidget(self.solenoid14_toggle.button)
-
-        self.clearData_button = QtGui.QPushButton(self.buttonsFrame, text="Clear Data")
-        self.clearData_button.clicked.connect(self.clearData)
-        self.clearData_button.setGeometry(0, 0, 200, 100)
-
-        self.show()
-
-    def slider(self, value):
-        print(value)
-
-    def clearData(self):
-        self.plotSet = PlotSet(self)
-        self.plotSet.plot.update([], [])
+        # Logger
+        self.logger = Logger()
+        self.logger.open()
+        
+        # #Status Indicators
+        # self.indicatorsFrame = tk.Frame(self.rightFrame)
+        # self.indicatorsFrame.grid(row=1, column=0 ,columnspan=2, pady=25)
 
     def close(self):
-        print("Closing Application")
+        print("Closing Application") 
         if (hasattr(self, 'arduino')):
             self.arduino.close()
         self.logger.close()
         self.destroy()
 
     # The main code loop that runs in the background of the window (Repeatedly after "frequency" milliseconds)
-    def loop(self, frequency):
+    def loop(self):
         try:
             time = datetime.now()
-
+            global lastStatus
             # Check for received data
             if (self.arduino.recvData()):
-
-                # Update Plots
-                self.plotSet.update(time, self.arduino.data)
-
-                currentTime = datetime.now()
-                # Update Readouts if 500ms have passed
-                if ((currentTime - self.lastDisplayTime).total_seconds() * 1000 > self.refreshRate):
-                    self.temperatureReadout.setText(f'{round(self.arduino.data.T1, 2)}')
-
+                output = ""
+                for x in self.indicators:
+                    if x[0] != 0:
+                        if self.arduino.data.status_int & x[3]:
+                            output += (x[1] + " ")
+                            x[0].config(bg = x[2])
+                        else:
+                            x[0].config(bg = "white")
+                            
+                if lastStatus != self.arduino.data.status_int:
+                    print(output)
+                    lastStatus = self.arduino.data.status_int
+            
+                elapsed_seconds = (time - self.start_time).total_seconds()
+                self.data = np.append(self.data, [[elapsed_seconds],[self.arduino.data.P1],[self.arduino.data.P2],[self.arduino.data.P3],[self.arduino.data.P4],[self.arduino.data.T1],[self.arduino.data.L1]], axis=1)
+                # Update Readouts if 50ms have passed
+                global curves, plot
+                curves[0].setData(self.data[0], self.data[6])
+                curves[1].setData(self.data[0], self.data[1])
+                curves[2].setData(self.data[0], self.data[2])
+                curves[3].setData(self.data[0], self.data[3])
+                curves[4].setData(self.data[0], self.data[4])
+                curves[5].setData(self.data[0], self.data[5])
+                plot.setTitle(f'Elapsed Time: {elapsed_seconds:.1f} seconds')
                 # Log data to file
                 self.logger.write(time, self.arduino.data)
 
@@ -163,10 +109,7 @@ class App(QtGui.QMainWindow):
             print(f"Error Parsing Arduino data: '{e}'")
             import traceback
             traceback.print_exc()
-
-        # Run Loop again after "frequency" milliseconds
-        self.after(frequency, self.loop, frequency)
-
+        
 """ Logger is used to log the data to a dataFile while the application is running
 """
 class Logger():
@@ -210,11 +153,11 @@ class Arduino():
             self.link = txfer.SerialTransfer(serialPort)
             self.link.open()
             time.sleep(2)
-
+            
         except Exception:
             import traceback
             traceback.print_exc()
-
+            
             try:
                 self.link.close()
             except Exception:
@@ -223,7 +166,7 @@ class Arduino():
         self.control_int = 0
         self.control_list = [0] * 32#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
         self.data = Data()
-
+    
     def close(self):
         try:
             self.link.close()
@@ -233,30 +176,29 @@ class Arduino():
     def recvData(self):
         if self.link.available():
             recSize = 0
+            
+            self.data.L1 = self.link.rx_obj(obj_type='H', start_pos=recSize)
+            recSize += 2
+            
+            self.data.P1 = self.link.rx_obj(obj_type='H', start_pos=recSize)
+            recSize += 2
+            
+            self.data.P2 = self.link.rx_obj(obj_type='H', start_pos=recSize)
+            recSize += 2
+            
+            self.data.P3 = self.link.rx_obj(obj_type='H', start_pos=recSize)
+            recSize += 2
+            
+            self.data.P4 = self.link.rx_obj(obj_type='H', start_pos=recSize)
+            recSize += 2
+            
+            self.data.T1 = self.link.rx_obj(obj_type='H', start_pos=recSize)
+            self.data.T1 = self.data.T1/10
+            recSize += 2
 
-            self.data.millisSince = self.link.rx_obj(obj_type='i', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['i']
-
-            self.data.L1 = self.link.rx_obj(obj_type='f', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
-
-            self.data.P1 = self.link.rx_obj(obj_type='f', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
-
-            self.data.P2 = self.link.rx_obj(obj_type='f', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
-
-            self.data.P3 = self.link.rx_obj(obj_type='f', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
-
-            self.data.P4 = self.link.rx_obj(obj_type='f', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
-
-            self.data.T1 = self.link.rx_obj(obj_type='f', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['f']
-
-            self.data.Safe = self.link.rx_obj(obj_type='b', start_pos=recSize)
-            recSize += txfer.STRUCT_FORMAT_LENGTHS['b']
+            self.data.status_int = self.link.rx_obj(obj_type='I', start_pos=recSize)
+            recSize += 4
+            
             return True
         
         elif self.link.status < 0:
@@ -269,7 +211,7 @@ class Arduino():
             else:
                 print('ERROR: {}'.format(self.link.status))
         return False
-
+    
     def sendCommand(self, command):
         # Commands encoded as 3 digit numbers: 01-... for solenoid number, and 0-1 for off/on
         self.control_list[-int(command[0]+command[1])-1] = int(command[2])
@@ -281,8 +223,8 @@ class Arduino():
         except:
             import traceback
             traceback.print_exc()
-
-            self.link.close()
+               
+            self.link.close()    
 
 """ The ArduinoSim is used to allow the application to be tested (for development) without
 needing to connect an arduino
@@ -298,12 +240,12 @@ class ArduinoSim():
 
         self.lastSendTime = datetime.now()
 
-
+    
     def close(self):
-        print(f"Sim close()")
+        print(f"Sim close()") 
         pass
 
-    def recvData(self):
+    def recvData(self): 
         currentTime = datetime.now()
 
         if (((currentTime - self.lastSendTime).total_seconds() * 1000) > 100):
@@ -318,10 +260,10 @@ class ArduinoSim():
             self.lastSendTime = currentTime
             return True
         return False
-
+    
     def sendCommand(self, command):
-        print(f"Sim send command: {command}")
-
+        print(f"Sim send command: {command}") 
+  
 """ The main code block is run when this python file is run and starts the application"""
 if __name__ == "__main__":
 
@@ -334,8 +276,8 @@ if __name__ == "__main__":
 
     # Refresh Rate is how often the UI readouts are updated to allow for better readability
     refreshRate = 250 # The UI updates every 500 ms
-    app = App(arduino, refreshRate, debug = False)
-    
-    # The app main loop will run repeatedly in a loop with at 10ms delay to allow the UI to update
-    app.loop(10)
-    app.mainloop()
+    app = App(arduino, refreshRate, debug = True)
+    timer = QtCore.QTimer()
+    timer.timeout.connect(app.loop)
+    timer.start(50)
+    pg.exec()
